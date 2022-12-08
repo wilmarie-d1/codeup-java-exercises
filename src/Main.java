9da134235cefdc394833bc4ff481d53cd30b3f5e
Wilmarie De La Cruz <wilmarie.delacruz@gmail.com>
2022-12-08 12:16:29 -0600

    adding starting place

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("hello world!");
+    }
+}
+//main method shortcut
+//main (tab)
+//psvm(tab/enter)
+
+//System.out.println shortcut
+//sout
