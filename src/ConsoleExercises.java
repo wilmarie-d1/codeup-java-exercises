ddfbfec15f964f9690bd64285a2c6afd64034249
Wilmarie De La Cruz <wilmarie.delacruz@gmail.com>
2022-12-13 10:31:34 -0600

    exercise

--- src/ConsoleExercises.java
+++ src/ConsoleExercises.java
@@ -4,7 +4,7 @@ public class ConsoleExercises {
     public static void main(String[] args) {
 
 //        Copy this code into your main method:
-//double pi = 3.14159;
+double pi = 3.14159;
 ////Write some Java code that uses the variable pi to output the following:
 ////The value of pi is approximately 3.14.
 ////Don't change the value of the variable; instead, reference one of the links above and use System.out.format to accomplish this.
@@ -12,19 +12,19 @@ public class ConsoleExercises {
 ////        Explore the Scanner Class.
 ////        Prompt a user to enter a integer and store that value in an int variable using the nextInt method.
 Scanner scanner = new Scanner(System.in); //scanner object
-//    System.out.print("Enter an integer please: ");
-//    int userInput = scanner.nextInt();
-//    System.out.println("You entered: " + userInput );
+    System.out.print("Enter an integer please: ");
+    int userInput = scanner.nextInt();
+    System.out.println("You entered: " + userInput );
 
 //        What happens if you input something that is not an integer?
        // It does not accept it
 
 //        Prompt a user to enter 3 words, and store each of them in a separate variable. Then, display them back in the console, each on a newline.
-//        System.out.println("Enter three words: ");
-//        String userInput1 = scanner.next();
-//        String userInput2 = scanner.next();
-//        String userInput3 = scanner.next();
-//        System.out.printf("You entered: %s%n %s%n %s%n", userInput1, userInput2, userInput3);
+        System.out.println("Enter three words: ");
+        String userInput1 = scanner.next();
+        String userInput2 = scanner.next();
+        String userInput3 = scanner.next();
+        System.out.printf("You entered: %s%n %s%n %s%n", userInput1, userInput2, userInput3);
 ////
 //                What happens if you enter less than 3 words?
 //                What happens if you enter more than 3 words?
@@ -32,8 +32,8 @@ Scanner scanner = new Scanner(System.in); //scanner object
 //                Prompt a user to enter a sentence, then store that sentence in a String variable using the next method. Then, display that sentence back to the user.
 //        System.out.println("Enter a sentence: ");
 //        String inputStr = scanner.nextLine();
-//        System.out.println("You wrote: " + inputStr);
-//
+//        System.out.printf("You wrote: %s ", inputStr);
+
 //        do you capture all the words?
 //                Rewrite the above example using the nextLine method.
 
@@ -46,11 +46,13 @@ Scanner scanner = new Scanner(System.in); //scanner object
 //Display the area and perimeter of that classroom.
 //
 //The area of a rectangle is equal to the length times the width, and the perimeter of a rectangle is equal to 2 times the length plus 2 times the width.
-            System.out.print("What is the width of the classroom?");
-            String userWidth = scanner.nextLine();
+        Scanner scanner1 = new Scanner(System.in); //scanner object
+
+        System.out.print("What is the width of the classroom?");
+            String userWidth = scanner1.nextLine();
             int width = Integer.parseInt(userWidth);
             System.out.print("What is the length of the classroom?");
-            String userLength = scanner.nextLine();
+            String userLength = scanner1.nextLine();
             int length = Integer.parseInt(userLength);
             int area = (length * width);
             int perimeter = 2 * (length + width);
