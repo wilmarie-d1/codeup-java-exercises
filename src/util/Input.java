03f6e4d05dc09e351e00148cec990bb9f7c121f7
Wilmarie De La Cruz <wilmarie.delacruz@gmail.com>
2023-01-02 15:24:17 -0600

    exercise

--- src/util/Input.java
+++ src/util/Input.java
@@ -1,17 +1,46 @@
 package util;
- import java.util.Scanner;
 
- public class Input {
+import java.util.Scanner;
+
+public class Input {
+
     private static Scanner scanner = new Scanner(System.in);
-    public static  String getString() {
+
+    public static String getString(){
         return scanner.nextLine();
     }
+
     public static boolean yesNo(){
         String userInput = scanner.nextLine();
-        return userInput.equalsIgnoreCase("yes") || userInput.equalsIgnoreCase("y");
+        return userInput.equalsIgnoreCase("yes") || userInput.equalsIgnoreCase("y") || userInput.equalsIgnoreCase("si");
     }
 
+    public static int getInt(int min, int max){
+        System.out.printf("Enter a number between %d and %d", min, max);
+        int userNumber = scanner.nextInt();
+        if(userNumber < min || userNumber > max){
+            getInt(min, max);
+        }
+        return userNumber;
 
+    }
 
+    public static int getInt(){
+        return scanner.nextInt();
+    }
+
+    public static double getDouble(double min, double max){
+        System.out.printf("Enter a number between %s and %s", min, max);
+        double userNumber = scanner.nextDouble();
+        if(Double.compare(userNumber, min) < 0 || Double.compare(userNumber, max) > 0 ){
+            getDouble(min, max);
+        }
+        return userNumber;
+    }
+
+    public static double getDouble(){
+        return scanner.nextDouble();
+
+    }
 
 }
