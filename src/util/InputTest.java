e690050074208981d6a6ae08ef0e6278ece59c85
Wilmarie De La Cruz <wilmarie.delacruz@gmail.com>
2023-01-02 15:25:03 -0600

    input test exercise

--- src/util/InputTest.java
+++ src/util/InputTest.java
@@ -1,12 +1,14 @@
 package util;
+import java.util.Scanner;
 
 public class InputTest {
-public static void main(String[] args) {
-    System.out.println(Input.getString());
-    System.out.println(Input.getString());
-    System.out.println(Input.getString());
-    System.out.println(Input.getString());
-    System.out.println(Input.getString());
-    System.out.println(Input.getString());
-}
+    public static void main(String[] args){
+        System.out.println(Input.getString());
+        System.out.println(Input.yesNo());
+        System.out.println(Input.getInt(3, 50));
+        System.out.println(Input.getInt());
+        System.out.println(Input.getDouble(10.05, 20.05));
+        System.out.println(Input.getDouble());
+
+    }
 }
