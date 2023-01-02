49df2ff861d45605e62ddd9bf29f42fa0478c80f
Wilmarie De La Cruz <wilmarie.delacruz@gmail.com>
2023-01-02 15:26:45 -0600

    circle exercise

--- src/shapes/CircleApp.java
+++ src/shapes/CircleApp.java
@@ -1,4 +1,13 @@
 package shapes;
+import util.Input;
 
 public class CircleApp {
+    public static void main(String[] args){
+        System.out.println("Enter a radius for the circle: ");
+        double userRadius = Input.getDouble();
+        Circle circle = new Circle(userRadius);
+        double area = circle.getArea();
+        double circumference = circle.getCircumference();
+        System.out.printf("The area of the circle is %s and the circumference is %s.", area, circumference);
+    }
 }
