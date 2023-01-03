711b0d34169c45808e98027f1f1742048b5462d4
Wilmarie De La Cruz <wilmarie.delacruz@gmail.com>
2023-01-03 13:57:29 -0600

    inheretance/polymorphism exercise

--- src/shapes/Square.java
+++ src/shapes/Square.java
@@ -4,15 +4,18 @@ public class Square extends Rectangles {
         protected double side;
     public Square(double side) {
         super(side, side);
+        this.side = side;
     }
 
     @Override
     public double getPerimeter() {
+        System.out.println("get perimeter from square class");
         return 4 * side;
     }
 
     @Override
     public double getArea() {
+        System.out.println("get perimeter from rectangle class");
         return Math.pow(side, 2);
     }
 }
