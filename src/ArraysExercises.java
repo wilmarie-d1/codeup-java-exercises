8dac8da41de5c9487f9d2af227d69e77783c5c33
Wilmarie De La Cruz <wilmarie.delacruz@gmail.com>
2023-01-24 21:52:02 -0600

    java exercise

--- src/ArraysExercises.java
+++ src/ArraysExercises.java
@@ -1,29 +1,35 @@
 import java.util.Arrays;
 
 public class ArraysExercises {
-    public static void main(String[] args) {
-        Person[] people = new Person[3];
-        people[0] = new Person("Koala");
-        people[1] = new Person("Chico");
-        people[2] = new Person("Samantha");
-        people[3] = new Person(" Mary");
 
-        for (Person person : people) {
+    public static void main(String[] args) {
+        int[] numbers = {1, 2, 3, 4, 5};
+        System.out.println(Arrays.toString(numbers));
+
+        Person[] people = new Person[3];
+        Person person1 = new Person("joe");
+        Person person2 = new Person("jay");
+        Person person3 = new Person("nikki");
+        Person person4 = new Person("will");
+        Person person5 = new Person("bill");
+
+        people[0] = person1;
+        people[1] = person2;
+        people[2] = person3;
+
+        for(Person person: people){
             System.out.println(person.getName());
         }
-//Create a static method named addPerson. It should accept an array of Person objects, as well as a single person object to add to the passed array. It should return an array whose length is 1 greater than the passed array, with the passed person object at the end of the array.
 
-//addPerso
-//public static Person [] addPerson(Person[] oldArr, Person newPerson){
-//Person[] newArr = Arrays.copyOf(oldArr, oldArr.length + 1);
-//newArr[oldArr.length-1] = newPerson;
-//newArr[3] = newPerson;
-//    System.out.println(newArr[oldArr.length-1].getName());
-//    return newArr;
+        addPerson(people, person4);
+        addPerson(people, person5);
+    }
 
-}
+    public static Person[] addPerson(Person[] oldArr, Person newPerson){
+        Person[] newArr = Arrays.copyOf(oldArr, oldArr.length + 1);
+        newArr[oldArr.length - 1] = newPerson;
 
-
-
-
+        System.out.println(newArr[oldArr.length - 1].getName());
+        return newArr;
+    }
 }
