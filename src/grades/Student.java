7899b281e0c347a1d6b4b64daf0018e29df128a1
Wilmarie De La Cruz <wilmarie.delacruz@gmail.com>
2023-02-06 10:34:13 -0600

    setting up class

--- src/grades/Student.java
+++ src/grades/Student.java
@@ -1,4 +1,16 @@
 package grades;
 
-public class Student {
+public abstract class Student {
+    // returns the student's name
+    public String getName() {
+        return null;
+    }
+
+    // adds the given grade to the grades property
+    public abstract void addGrade(int grade);
+
+    // returns the average of the students grades
+    public double getGradeAverage() {
+        return 0;
+    }
 }
--- src/movies/Movie.java
+++ src/movies/Movie.java
@@ -1,4 +1,29 @@
 package movies;
 
 public class Movie {
+
+    private String name;
+    private String category;
+
+    public Movie(String name, String category){
+        this.name = name;
+        this.category = category;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+}
 }
